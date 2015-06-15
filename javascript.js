e80c77260d0b8987d93ec9af65ac70dee86b9bcf
Cindy Liu <cindyliu@berkeley.edu>
2015-06-15 14:31:26 -0700

    initial image positioning 2

---
+++ javascript.js
@@ -0,0 +1,8 @@
+// document.getElementById('cindy').onmouseover = function() {
+// 	document.getElementById('cindy_jump').style.display = 'inline';
+// }
+
+// document.getElementById('cindy').onmouseover = function() {
+// 	document.getElementById('cindy_jump').style.display = 'none';
+// }
+
